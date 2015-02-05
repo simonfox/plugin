f185ea1df4b9d2f14062a8c1ef64ab586291cd0c
Simon Fox <simon.m.fox@gmail.com>
2015-02-05 14:20:33 +1300

    look up aurelia object and use container

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,16 @@
 import {Router} from 'aurelia-router';
+import {Boom} from './boom';
 
 export class Bootstrap {
   //static inject() { return [Router]; }
   constructor() {
     
-	var router = document.body.aurelia.container.get(Router);
+    var container = document.body.aurelia.container;
+    container.registerTransient(Boom, x => return new Boom());
+	
+	var router = container.get(Router);
 	router.addRoute({ route: 'boom', moduleId: 'boom', title:'Boom!!!'});
+	
 	router.navigate("boom");
   }
 }
