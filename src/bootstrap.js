c485f49cde0acdab74bf32fa5a0a438f29f568b2
Simon Fox <simon.m.fox@gmail.com>
2015-02-10 10:57:48 +1300

    hacking

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,16 +1,12 @@
-import {Router} from 'aurelia-router';
-import {Boom} from './boom';
 
 export class Bootstrap {
+
   constructor() {
     
     var container = document.body.aurelia.container;
-    //container.registerTransient(Boom, Boom);
-	
 	var router = container.get(Router);
+
 	router.addRoute({ route: 'boom', moduleId: 'boom', title:'Boom!!!'});
-	
 	router.navigate("boom");
-	//router.navigate("welcome");
   }
 }
